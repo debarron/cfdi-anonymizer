8976f4499bede41a8757e156bf729bd265ea391c
Daniel Lopez <de.lopezbarron@gmail.com>
2022-03-30 09:35:46 -0600

    feature(adding-extra-input): Require sensitive filter as input
    Add another argument to the script to ask for the sensitive keyword filter

--- anonymizer.sh
+++ anonymizer.sh
@@ -2,13 +2,17 @@
 # This script taks the keywords file and substitues
 # each attribute in the file with a string. The reason
 # is to remove sensitive information from the XML data
+# INPUT
 # $1 xml invoice file
+# $2 sensitive keyword filter
 
 original="$1"
+sensitive_filter="$2"
+
 temp_1="tmp_1.xml"
 cat $original > $temp_1
 
-for keyword in $(cat "./sensitive_field_filter.txt")
+for keyword in $(cat "$sensitive_filter")
 do
 	key=$(echo $keyword | cut -d, -f1)
 	value=$(echo $keyword | cut -d, -f2)
